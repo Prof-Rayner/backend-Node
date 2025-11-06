18dfecb833d9c22043c8d6cba38853f1a56bb539
Prof. Jeff Rayner <jeff.rfsimoes@senacsp.edu.br>
2025-11-06 16:52:29 -0300

    feat: criado a rota de tarefas. #1

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { Request, Response, NextFunction } from "express";
+import router from "./routes/tarefasRouter";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-
-
+app.use("/tarefas", router);
 
 export default app;
---
+++ src/routes/tarefasRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", ()=>console.log('pegar todas as tarefas'))
+router.get("/:id", ()=>console.log('pegar uma tarefa'))
+router.post("/", ()=>console.log('cadastrar uma tarefa'))
+router.put("/:id", ()=>console.log('atualizar uma tarefa'))
+router.delete("/:id", ()=>console.log('deltar uma tarefa'))
+
+export default router;
