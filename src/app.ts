4f2b20ff24766576fddc7f6b833f0caf50177faf
Prof. Jeff Rayner <jeff.rfsimoes@senacsp.edu.br>
2025-11-06 15:24:43 -0300

    Feat|Test: escrito exemplo de acesso ao conteudo. #3

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,33 @@ import express from "express";
 import { Request, Response, NextFunction } from "express";
 
 const app = express();
+app.use(express.json())
 
-
-// rota generica
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!!");
+//rota parametro:  /parametro/seuNome
+app.get("/parametro/:nome", (req: Request, res: Response, next: NextFunction) => {
+  const nome = req.params.nome;
+  console.log("Rota de parametro - Cliente digitou:", nome);
+  res.send(`voce digitou o nome: ${nome}`);
 });
 
-// rota de erro
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-  res.status(500).send("Erro na requisição");
+//rota query(opcional): /query ? inicio=2025&fim=2026&qt=2
+app.get("/query", (req: Request, res: Response, next: NextFunction) => {
+  const nome = req.query.nome;
+  console.log("Cliente digitou:", nome);
+  res.send(`voce digitou o nome: ${nome}`);
+});
+
+//rota parametros no corpo - /corpo
+app.get("/corpo", (req: Request, res: Response, next: NextFunction) => {
+  const nome = req.body.nome;
+  console.log("Variavel dentro do copo - Cliente digitou:", nome);
+  res.send(`Variavel dentro do copo - voce digitou o nome: ${nome}`);
+});
+
+
+app.get("/exemplo", (req: Request, res: Response, next: NextFunction) => {
+  console.log("aconteceu algo!!!!!");
+  res.send("rodou tudo serto!!");
 });
 
 export default app;
